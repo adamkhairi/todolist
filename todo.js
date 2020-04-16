62884ededc08c0e4f94cc4276de86fc628fbe2e0
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-04-16 01:12:07 +0100

    In Progress

--- todo.js
+++ todo.js
@@ -7,109 +7,116 @@ const btn = document.querySelector('.btn');
 const search = document.querySelector('.search input');
 gPop.style.display = "none";
 
-
 /***************reusable function********************/
 
 /* Function pour l'alert et le popup qui va etre afficher (time control)*/
-function start(duree)
-{
-var o=document.getElementById("sp");
-if(duree > 0)
-{
-o.innerHTML = duree;
-gPop.style.display = "block";
-setTimeout("start("+duree+" -1)", 1000);
-}
-else
-{
-   alert("enter a valid to do");
-o.innerHTML ="Au revoir";
-gPop.style.display="none";
-popup.style.visibility ="hidden";
-
-}};
-
+function start(duree) {
+	var o = document.getElementById("sp");
+	if (duree > 0) {
+		o.innerHTML = duree;
+		gPop.style.display = "block";
+		setTimeout("start(" + duree + " -1)", 1000);
+	} else {
+		alert("enter a valid to do");
+		o.innerHTML = "Au revoir";
+		gPop.style.display = "none";
+		popup.style.visibility = "hidden";
+		
+	}
+};
 
 /* Function Creation dynamique du POPUP */
 
-function create(){
-   const div = document.createElement('div');
-   div.classList.add('popup-close');
-   div.setAttribute('id','closing');
-   const text = document.createTextNode('X');
-   div.appendChild(text);
-   popup.append(div);
-   const div2 = document.createElement('div');
-   div2.classList.add('popup-content');
-   const html = `
+function create() {
+	const div = document.createElement('div');
+	div.classList.add('popup-close');
+	div.setAttribute('id', 'closing');
+	const text = document.createTextNode('X');
+	div.appendChild(text);
+	popup.append(div);
+	const div2 = document.createElement('div');
+	div2.classList.add('popup-content');
+	const html = `
    <span id="sp">1</span>
    <h2>Fill the Input</h2>
    <p>Don't forget</p>
    <a href="#">Return</a>`;
-   div2.innerHTML=html;
-   popup.append(div2); 
-   
+	div2.innerHTML = html;
+	popup.append(div2);
+	
 }
 
 /* Function generation dynamique des TODOS */
 
-const generateTemp = todo =>{
-   const html = `
+const generateTemp = todo => {
+	const html = `
    <li class="list-group-item d-flex justify-content-between align-items-center">
              <span>${todo}</span>
              <i class="fas fa-trash delete"></i>
             </li>
-   `;  
-   list.innerHTML += html;
+   `;
+	list.innerHTML += html;
 };
-
 
 /* function pour controller l'evenement et pour ne pas etre repeté à chaque clique */
 function onetime(node, type, callback) {
-
-	node.addEventListener(type, function(e) {
 	
+	node.addEventListener(type, function (e) {
+		
 		e.target.removeEventListener(e.type, arguments.callee);
-
+		
 		return callback(e);
 	});
 }
 
-onetime(gPop,'click',handler);
+onetime(gPop, 'click', handler);
 
-    function handler(e){
-         
-      if(e.target.id='closing'){
-   
-         gPop.style.display ="none";
-   }
+function handler(e) {
+	
+	if (e.target.id = 'closing') {
+		
+		gPop.style.display = "none";
+	}
 }
 
 /***************Fin reusable function********************/
 
-
-
-
 /************* Adding TO DO**************/
 
 //Eventlistner Add TODOS
-btn.addEventListener('click',e =>{
- 
-  
+btn.addEventListener('click','keyup', e => {
+	
+	let inputt;
+	inputt = document.getElementById('textt');
+	if (!inputt.value) {
+		
+		create();
+		alert('rggg');
+		
+	} else {
+		
+		generateTemp(inputt.value);
+		inputt.value = "";
+	}
+	
 });
 
 /************* Fin Adding TO DO**************/
 
-
-
 /*************Deleting  TO DO**************/
-list.addEventListener('click',e =>{
 
+list.addEventListener("click", e => {
+	//list.removeChild(list.lastChild);
+	let deletee;
+	deletee = document.querySelectorAll('.delete');
+	for (let i = 0; i < deletee.length; i++) {
+		deletee[i].addEventListener('click', function () {
+			deletee[i].parentNode.remove();
+		})
+	}
 });
 
 /************* Fin Deleting  TO DO**************/
-
-
 
 
 /************************************* SEARCH ITEM********************************************/
@@ -119,17 +126,13 @@ list.addEventListener('click',e =>{
 
 // have keyup event 
 
-
-
-const retrieve = (term) =>{
-
-   //function pour faire un filtre i
-};  
-
+const retrieve = (term) => {
+	
+	//function pour faire un filtre i
+};
 
 //evenement de recherche des mots clés 
-search.addEventListener('keyup', () =>{
-  
+search.addEventListener('keyup', () => {
 
 })
 
